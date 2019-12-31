077e33ffdecd347ad07fd22d160d209f5c2276c6
janghyoseog <bestdevhyo1225@gmail.com>
2019-12-31 19:26:03 +0900

    feat: Add list result

--- src/main/java/com/rest/api/model/response/ListResult.java
+++ src/main/java/com/rest/api/model/response/ListResult.java
@@ -1,4 +1,12 @@
 package com.rest.api.model.response;
 
-public class ListResult {
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class ListResult<T> extends CommonResult {
+    private List<T> list;
 }
