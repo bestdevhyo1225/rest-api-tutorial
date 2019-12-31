60154c42e5ac86830506e49fd419cd9cc678dcc8
janghyoseog <bestdevhyo1225@gmail.com>
2019-12-31 19:27:17 +0900

    feat: Add response service using 'common result', 'single result', 'list result'

--- src/main/java/com/rest/api/service/ResponseService.java
+++ src/main/java/com/rest/api/service/ResponseService.java
@@ -1,4 +1,72 @@
 package com.rest.api.service;
 
+import com.rest.api.model.response.CommonResult;
+import com.rest.api.model.response.ListResult;
+import com.rest.api.model.response.SingleResult;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ResponseService {
+    // enum으로 API 요청 결과에 대한 code, message를 정의한다.
+    public enum CommonResponse {
+        SUCCESS(0, "성공했습니다."),
+        FAIL(-1, "실패했습니다.");
+
+        private int code;
+        private String message;
+
+        CommonResponse(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+        public int getCode() {
+            return this.code;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
+    // 결과 모델에 API 요청 성공 데이터 세팅해주는 메소드
+    private void setSuccessResult(CommonResult result) {
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMessage(CommonResponse.SUCCESS.getMessage());
+    }
+
+    // 성공 결과만 처리하는 메소드
+    public CommonResult getSuccessResult() {
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 실패 결과만 처리하는 메소드
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMessage(CommonResponse.FAIL.getMessage());
+        return result;
+    }
+
+    // 단일 결과를 처리하는 메소드
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 다중 결과를 처리하는 메소드
+    public <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
 }
